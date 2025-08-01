ef412c2828b45aacf9626a4294bac019e5e7a95d
Charles Bergeron <chuck.bergeron@gmail.com>
2025-08-01 16:45:30 -0700

    update translations and responsive banner

--- apps/app/src/components/RewardsBackdatedBanner.tsx
+++ apps/app/src/components/RewardsBackdatedBanner.tsx
@@ -4,9 +4,9 @@ export const RewardsBackdatedBanner = () => {
   const t_common = useTranslations('Common')
 
   return (
-    <div className='flex h-10 w-100 text-white bg-gradient-to-r from-blue-700 to-pt-purple-600 text-white shadow z-20  items-center justify-center px-4 py-3'>
+    <div className='flex h-14 w-100 text-white bg-gradient-to-r from-blue-700 to-pt-purple-600 text-white shadow z-20  items-center justify-center px-4 py-3'>
       <span className='mr-2'>⚡</span>
-      <div className='whitespace-nowrap text-center font-medium'>
+      <div className='leading-tight text-center font-light text-sm'>
         {t_common('rewardsBackdated')}
       </div>
       <span className='ml-2'>⚡</span>
